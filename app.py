9cc46398dccec1ac12529b69d3f52f1d4022f2dd
scret-black-backup <gerencialfacebook@gmail.com>
2025-09-03 17:48:55 +0000

    Altera a forma de servir o arquivo index.html para usar send_from_directory

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return send_from_directory(".", "index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
